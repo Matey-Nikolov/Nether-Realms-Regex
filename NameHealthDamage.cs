b2c4be76421f3e2163c6442605d95cbaaf034a86
Matey-Nikolov <matey.nikolov@mail.bg>
2021-03-22 18:48:41 +0200

    Add project files.

---
+++ NameHealthDamage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nether_Realms_Regex
+{
+    class NameHealthDamage
+    {
+        public string Name { get; set; }
+
+        public int Health { get; set; }
+
+        public double Damage { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Nether_Realms_Regex
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            //Dictionary<string, Dictionary<int, double>> nameHealthDamage = new Dictionary<string, Dictionary<int, double>>();
+
+            List<NameHealthDamage> nameHealthDamages = new List<NameHealthDamage>();
+
+            List<string> codeText = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            int health = 0;
+            double damage = 0;
+
+            foreach (string text in codeText)
+            {
+
+                string patternWordHealth = @"[A-za-z]+";
+                MatchCollection codeWordMatch = Regex.Matches(text, patternWordHealth);
+
+                string letter = "";
+                foreach (var chr in codeWordMatch)
+                    letter += chr.ToString();
+
+                foreach (char chr in letter)
+                    health += (int)(chr);
+
+                string patternDamage = @"[-?\d.?]+";
+                MatchCollection codeNumbers = Regex.Matches(text, patternDamage);
+
+
+                foreach (var digit in codeNumbers)
+                    damage += double.Parse(digit.ToString());
+
+                string paternMultiaOrDivision = @"[\/|*]+";
+                MatchCollection MultiAndDivision = Regex.Matches(text, paternMultiaOrDivision);
+
+                foreach (var item1 in MultiAndDivision)
+                {
+                    foreach (char item2 in item1.ToString())
+                    {
+                        if (item2 == '*')
+                        {
+                            damage *= 2;
+                        }
+                        else if (item2 == '/')
+                        {
+                            damage /= 2;
+                        }
+                    }
+                }
+
+                nameHealthDamages.Add(new NameHealthDamage()
+                {
+                    Name = text,
+                    Health = health,
+                    Damage = damage
+                });
+
+                health = 0;
+                damage = 0;
+            }
+
+            foreach (var NHD in nameHealthDamages
+                .OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{NHD.Name} - {NHD.Health} health, {NHD.Damage:f2} damage");
+            }
+
+        }
+    }
+}
